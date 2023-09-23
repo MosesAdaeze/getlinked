0373ad0987afcb252ee0c8923cb194cb1a418464
Kloss <108338208+MosesAdaeze@users.noreply.github.com>
2023-09-23 22:02:57 +0100

    Update
    FAQ / License policy

--- src/components/section4/page.js
+++ src/components/section4/page.js
@@ -12,7 +12,54 @@ export default function Section4() {
           We got answers to the questions that you might want to ask about
           getlinked Hackathon 1.0
         </p>
+
+        <div className={styles.faq_container}>
+        <div className={styles.faq_item}>
+          <div className={styles.faq_question}><a href="#">Can I work on a project I started before the hackathon?</a>
+          <img src="/add.png" alt="add" />
+          </div>
+          {/* <div className={styles.faq_answer}> Answer</div> */}
+        </div>
+
+        <div className={styles.faq_item}>
+          <div className={styles.faq_question}><a href="#">What happens if I need help during the hackathon?</a>
+          <img src="/add.png" alt="add" />
+          </div>
+          {/* <div className={styles.faq_answer}> Answer</div> */}
+        </div>
+
+        <div className={styles.faq_item}>
+          <div className={styles.faq_question}><a href="#">What happens if I don't have an idea for a project?</a>
+          <img src="/add.png" alt="add" />
+          </div>
+          {/* <div className={styles.faq_answer}> Answer</div> */}
+        </div>
+
+        <div className={styles.faq_item}>
+          <div className={styles.faq_question}><a href="#">Can I join a team or do I have to come with one?</a>
+          <img src="/add.png" alt="add" />
+          </div>
+          {/* <div className={styles.faq_answer}> Answer</div> */}
+        </div>
+
+        <div className={styles.faq_item}>
+          <div className={styles.faq_question}><a href="#">What happens after the hackathon ends</a>
+          <img src="/add.png" alt="add" />
+          </div>
+          {/* <div className={styles.faq_answer}> Answer</div> */}
+        </div>
+
+        <div className={styles.faq_item}>
+          <div className={styles.faq_question}><a href="#">Can I work on a project I started before the hackathon?</a>
+          <img src="/add.png" alt="add" />
+          </div>
+          {/* <div className={styles.faq_answer}> Answer</div> */}
+        </div>
       </div>
+
+    </div>
+
+
       <div className={styles.section__img}>
          <img src="/thinking-man.svg" alt="omo" />
       </div>
--- src/components/section8/page.js
+++ src/components/section8/page.js
@@ -12,7 +12,38 @@ export default function Section8() {
           Below are our privacy & policy, which outline a lot of goodies. its
           our aim to always take of our participant
         </p>
-      </div>
+
+        <div className={styles.policy_container}>
+        <div className={styles.policy_content}>
+          <p>
+            At getlinked tech Hackathon 1.0, we value your privacy and are committed to protecting your personal information.
+            This Privacy Policy outlines how we collect, use, disclose, 
+            and safeguard your data when you participate in our tech 
+            hackathon event. By participating in our event, you consent 
+            to the practices described in this policy.
+          </p>
+
+          <div className={styles.license}>
+            <h4>Licensing Policy</h4>
+            <p>Here are terms of our standard License:</p>
+
+            <div className={styles.license_list}>
+            <img src="/check.png" alt="check" />
+            <p>The Standard License grants you a non-exclusive right to navigate and register for our event</p>
+            </div>
+
+            <div className={styles.license_list}>
+            <img src="/check.png" alt="check" />
+            <p>You are licensed to use the item available at any free source sites, for your project developement</p>
+            </div>
+            </div>
+                
+            <div className={styles.button}><button className={styles.readMoreButton}>Read More</button></div>
+       </div>
+     </div>
+  </div>
+
+
       <div className={styles.section__img}>
         <img src="/padlock.png" alt="omo" />
       </div>
